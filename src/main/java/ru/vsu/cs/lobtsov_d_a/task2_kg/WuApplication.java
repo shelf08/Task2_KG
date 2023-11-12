e70f285b07ac5658233308718bdafc5db41b1a4a
shelf08 <vip.lobtsov@gmail.com>
2023-11-12 18:08:02 +0300

    fxml block of window

--- src/main/java/ru/vsu/cs/lobtsov_d_a/task2_kg/WuApplication.java
+++ src/main/java/ru/vsu/cs/lobtsov_d_a/task2_kg/WuApplication.java
@@ -12,7 +12,7 @@ public class WuApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WuApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 700);
-        stage.setTitle("Algorithm Wu");
+        stage.setTitle("Algorithm Wu Line");
         stage.setScene(scene);
         stage.show();
     }
